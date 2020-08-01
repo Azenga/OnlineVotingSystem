ad799df662dec4735cf3d921c08315937de86751
Azenga Kevin <azenga.kevin7@gmail.com>
2020-08-02 02:08:03 +0300

    Frontend scaffolding, environment setup and switch to bootstrap 5-alpha

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+
+Auth::routes(['register' => false]);
+
+Route::get('/home', 'HomeController@index')->name('home');
